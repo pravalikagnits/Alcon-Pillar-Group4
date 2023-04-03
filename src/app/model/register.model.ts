1dd4fb09e8e30e51d5b3ce4b7453ff631f1bae5a
IndrajithRam <indrajithcool@yahoo.com>
2023-04-02 21:24:36 -0400

    Login/Logout button show option

---
+++ src/app/model/register.model.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class Register {
+
+    public _id!: number;
+    public title!: string;
+    public owner!: string;
+    public sportType!: string;
+    public location!: string;
+    public teamCount!: number;
+    constructor() { }
+}
