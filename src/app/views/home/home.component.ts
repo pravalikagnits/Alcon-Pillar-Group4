46894e545d2164531a30a6a4d8a1a002daac28f0
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-04-08 19:52:22 -0400

    add condition  to show/hide create button

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public isUserLoggedIn: boolean = false;
+
+  ngOnInit(): void {
+    if (localStorage.getItem('id_token') != null) {
+      this.isUserLoggedIn = true;
+    }
+
+  }
 }
