88f3ec1ee377cb7490db420731b90b04b9aa4663
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-04-09 00:52:51 -0400

    add auth guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router:Router ) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if(localStorage.getItem("id_token") == null){
+      return false;
+    }
+
+    return true;
+  }
+  
+}
