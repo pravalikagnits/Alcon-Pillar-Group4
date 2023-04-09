6eed64bc9e969318847bb1d73ff751ed45417f7c
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-04-08 23:40:39 -0400

    add condition to button click

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,18 @@ export class HomeComponent implements OnInit {
 
   public isUserLoggedIn: boolean = false;
 
+  constructor(private router:Router){
+
+  }
+
   ngOnInit(): void {
     if (localStorage.getItem('id_token') != null) {
       this.isUserLoggedIn = true;
     }
 
+  }
+
+  public onClick():void{
+    this.isUserLoggedIn ? this.router.navigateByUrl('/displayAddTournament') :this.router.navigateByUrl('/login');
   }
 }
