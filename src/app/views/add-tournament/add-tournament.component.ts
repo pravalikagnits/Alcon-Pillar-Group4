99003cc1a123681ee6116afd918b5321049fa094
IndrajithRam <indrajithcool@yahoo.com>
2023-04-09 02:01:45 -0400

    tournament list changes

--- src/app/views/add-tournament/add-tournament.component.ts
+++ src/app/views/add-tournament/add-tournament.component.ts
@@ -9,7 +9,7 @@ import { TournamentRepository } from 'src/app/model/tournament.repository';
   templateUrl: './add-tournament.component.html',
   styleUrls: ['./add-tournament.component.css']
 })
-export class AddTournamentComponent  implements OnInit{
+export class AddTournamentComponent implements OnInit {
   editMode: boolean = false;
   tournament: Tournament = new Tournament();
   constructor(private repository: TournamentRepository, private router: Router, tournament: Tournament, activeRoute: ActivatedRoute) {
@@ -19,13 +19,16 @@ export class AddTournamentComponent  implements OnInit{
         .getTournament(activeRoute.snapshot.params['_id'])
         .subscribe((data) => (this.tournament = data));
     }
-   }
+  }
   ngOnInit(): void {
   }
   addTournament(form: NgForm) {
     if (form.valid) {
       this.repository.addTournament(this.tournament).subscribe(data => {
         console.log(data);
+        if (data) {
+          this.router.navigateByUrl("/tournamentsList");
+        }
       });
     }
     return;
